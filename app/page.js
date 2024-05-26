342b8dd91d8e8753274b54f199b5532dd60fb94c
Srishti Verma <136918651+sinceresrishti@users.noreply.github.com>
2024-05-26 12:45:20 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,66 @@
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setmainTask([...mainTask, {title, desc}]);
+    settitle("")
+    setdesc("")
+    console.log(mainTask)
+  }
+  
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i,1)
+    setmainTask(copyTask)
+  }
+
+  let renderTask = <h2>No Task Available</h2>
+  if(mainTask.length>0){
+  renderTask = mainTask.map((t,i) => {
+    return( 
+    <li key={i} className='flex items-center justify-between mb-5'>
+      <div className='flex justify-between mb-8 w-2/3'>
+      <h5 className='text-2xl font-semibold'>{t.title}</h5>
+      <h6 className='text-lg font-medium'>{t.desc}</h6> 
+    </div>
+    <button onClick={()=>{
+      deleteHandler(i)
+    }} className='bg-red-400 text-white px-4 py-4 rounded'>Delete</button>
+    </li>);
+  });
+}
+
+  return (
+    <>
+    <h1 className='bg-black text-white -bold text-center text=5xl font='>Srishti's Todo List</h1>
+    <form onSubmit={submitHandler}>
+      <input type="text" className='text-2xl border-zinc-800 m-8 px-4 py-2'
+      placeholder='Enter Task here'
+      value={title}
+      onChange={(e) => {
+        settitle(e.target.value)
+      }}/>
+      <input type="text" className='text-2xl border-zinc-800 m-8 px-4 py-2'
+      placeholder='Enter description here'
+      value={desc}
+      onChange={(e) => {
+        setdesc(e.target.value)
+      }}/>
+      <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Task</button>
+    </form>
+    <hr />
+    <div className='p-8 bg-slate-200'>
+       <ul>
+        {renderTask}
+       </ul>
+    </div>
+    </>
+  )
+}
+
+export default page
